063595f6b54ccd3f6420ad74a62b71876db547a0
anderson jackson <alystraj@yahoo.com>
2018-04-05 17:45:47 -0400

    Initial Commit

---
+++ GitTest/src/edu/tridenttech/cpt190/gittest/MainClass.java
@@ -0,0 +1,11 @@
+package edu.tridenttech.cpt190.gittest;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Goodbye World");
+	}
+
+}
